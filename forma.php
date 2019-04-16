8716f6a028e72ee938aadb80f9675f3894f8fc3b
BoBoJhon <shikablee@gmail.com>
2019-04-16 10:33:35 +0400

    END FORM

--- forma.php
+++ forma.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 <form action="link.php" method="POST" name="application">
-<input name="email" id="email" maxlength="20" placeholder="Email адрес" required />
-<input name="surname" type="text" maxlength="20" placeholder="Фамилия" required />
-<input name="name" type="text" maxlength="20" placeholder="Имя" required/>
-<input name="Patronymic" maxlength="20" placeholder="Отчество" required />
-<input name="telephone" type="Tel" maxlength="20" placeholder="Телефон" required />
-<input name="password" type="password" maxlength="20" placeholder="Пароль" required/>
-<input name="repeat_password" type="repeat_password" maxlength="20" placeholder="Подтверждение пароля" required/>
+<input name="email" id="email" maxlength="100" placeholder="Email адрес" required />
+<input name="surname" type="text" maxlength="100" placeholder="Фамилия" required />
+<input name="name" type="text" maxlength="100" placeholder="Имя" required/>
+<input name="Patronymic" maxlength="100" placeholder="Отчество" required />
+<input name="telephone" type="Tel" maxlength="100" placeholder="Телефон" required />
+<input name="password" type="password" maxlength="100" placeholder="Пароль" required/>
+<input name="repeat_password" type="repeat_password" maxlength="100" placeholder="Подтверждение пароля" required/>
 <button class="applicationButton" type="submit" form="application">Продолжить</button>
 </form>
 </body>
