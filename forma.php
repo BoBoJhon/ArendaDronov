402dc85f8f8ccd90f4be35bdde443d823d8a6ddd
BoBoJhon <shikablee@gmail.com>
2019-04-16 10:28:43 +0400

    Forma

---
+++ forma.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<form action="link.php" method="POST" name="application">
+<input name="email" id="email" maxlength="20" placeholder="Email адрес" required />
+<input name="surname" type="text" maxlength="20" placeholder="Фамилия" required />
+<input name="name" type="text" maxlength="20" placeholder="Имя" required/>
+<input name="Patronymic" maxlength="20" placeholder="Отчество" required />
+<input name="telephone" type="Tel" maxlength="20" placeholder="Телефон" required />
+<input name="password" type="password" maxlength="20" placeholder="Пароль" required/>
+<input name="repeat_password" type="repeat_password" maxlength="20" placeholder="Подтверждение пароля" required/>
+<button class="applicationButton" type="submit" form="application">Продолжить</button>
+</form>
+</body>
+</html>
