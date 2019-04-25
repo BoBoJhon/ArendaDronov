7a81d145dfbe0cd89b4b1a481d18d4711d735cb3
Sn0wN1nja <romohov.1918@yandex.ru>
2019-04-25 09:11:51 +0400

    Order?

--- DroneMenu.php
+++ DroneMenu.php
@@ -11,7 +11,7 @@
     <body>
     <?php
     $id=$_GET['id'];
-    $json=file_get_contents('http://Babyshev/api/server.php?uid=1&key=qwerty&query=select&table=Copters&fields=id,model,battery,id_blades,cost,camera_characteristic,advisory_services,range_flight,flight_time,max_speed&values=%22mavic%22,%221000V%22,%225000%22,%22topchick%22,%22yes%22,%223002%22,%2250%22,%221302%22,%22123%22');
+    $json=file_get_contents('http://id9333138_deal/api/server.php?uid=1&key=qwerty&query=select&table=Copters&fields=id,model,battery,cost,camera_characteristic&values=%22mavic%22,%221000V%22,%225000%22,%22topchick%22,%22yes%22,%223002%22,%2250%22,%221302%22,%22123%22');
     echo $json;
     $result=json_decode($json, TRUE);
     echo "<br>";
@@ -22,14 +22,8 @@
     echo $value['battery'];
     echo $value['cost'];
     echo $value['camera_characteristic'];
-    echo $value['advisory_services'];
-    echo $value['range_flight'];
-    echo $value['flight_time'];
-    echo $value['obstacle_flying_system'];
-    echo $value['Tracking_moving_objects'];
-    echo $value['Weight'];
-    echo $value['Maximum_flight_above_sea_level'];
-    echo $value['memory'];
+
+    
 
 
     ?>
